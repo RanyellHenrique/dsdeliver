8899b8004c40633cebb54ed5f771fb4a065d0611
RanyellHenrique <ranyellhenrique@gmail.com>
2021-01-09 15:21:35 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { OrderPayload } from './pages/Orders/types';
 
 
-const API_URL = 'https://ranyell-sds2.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export const fetchProducts = () => {
